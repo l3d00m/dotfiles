97cb0b36e15da556ebc69d09b7ce70fb45f9fff2
Thomas <substantialimpulse@pm.me>
2022-01-23 20:31:25 +0100

    tmux optimizations

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
 if hash pacman 2>/dev/null ; then
-    sudo pacman -Sy thefuck exa fzf zsh tmux
+    sudo pacman -Sy thefuck exa fzf zsh tmux xclip
+    yay -Su ccat
 elif hash apt-get 2>/dev/null ; then
-    sudo apt install -y thefuck exa fzf zsh tmux
+    sudo apt install -y thefuck exa fzf zsh tmux xclip
 fi
 
-sudo chsh -s $(which zsh) "$USER"
+sudo chsh -s $(which zsh) $(whoami)
