4d5f8fcf6bf9524bfdbdda18c27387f7ed2d3d42
Thomas <substantialimpulse@pm.me>
2022-01-24 18:54:41 +0100

    add .local/bin to PATH, use ranger with ctrl+o

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 if hash pacman 2>/dev/null ; then
-    sudo pacman -Sy thefuck exa fzf zsh tmux xclip
+    sudo pacman -Sy thefuck exa fzf zsh tmux xclip ranger
     yay -Su ccat
 elif hash apt-get 2>/dev/null ; then
-    sudo apt install -y thefuck exa fzf zsh tmux xclip
+    sudo apt install -y thefuck exa fzf zsh tmux xclip ranger
 fi
 
 sudo chsh -s $(which zsh) $(whoami)
