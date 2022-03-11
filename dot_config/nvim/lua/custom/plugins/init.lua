e1fff8e77d7937d218d5596ee77a01887c46c8b4
Thomas <substantialimpulse@pm.me>
2022-03-11 23:56:36 +0100

    vim plugins

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,8 @@ return {
   {"lervag/vimtex"},
   {"rhysd/vim-grammarous"},
   {"ntpeters/vim-better-whitespace"},
+  {"vim-pandoc/vim-pandoc-syntax"},
+  {"vim-pandoc/vim-pandoc"},
+  {"tpope/vim-obsession"},
+  {"tpope/vim-fugitive"},
 }
