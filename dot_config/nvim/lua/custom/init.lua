04bb532722abb4c4346b80dbe9d66f9f948ef899
Thomas <substantialimpulse@pm.me>
2022-11-03 20:56:23 +0100

    old changes

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -6,11 +6,9 @@
 local map = require("core.utils").map
 
 map("i", "jk", "<ESC>")
--- map("n", "<leader>q", ":q <CR>")
 
 -- NOTE: the 4th argument in the map function is be a table i.e options but its most likely un-needed so dont worry about it
 local opt = vim.opt
 opt.linebreak = true
-opt.timeoutlen = 250
+opt.timeoutlen = 350
 
-vim.g.transparent_enabled = true
