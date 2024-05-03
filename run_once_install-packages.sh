b71b5668bffc503f7212f7d414f7f6abdd84aa74
Thomas <substantialimpulse@pm.me>
2024-05-03 17:33:38 +0200

    setup formatters

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-if hash pacman 2>/dev/null ; then
-    sudo pacman -S yay thefuck exa fzf zsh tmux xclip ranger neovim ripgrep python-pynvim
-    yay -S ccat ttf-meslo-nerd-font-powerlevel10k fpp-git urlview
-elif hash apt-get 2>/dev/null ; then
-    sudo apt install -y thefuck exa fzf zsh tmux xclip ranger
+if hash pacman 2>/dev/null; then
+	sudo pacman -S yay thefuck exa fzf zsh tmux xclip ranger neovim ripgrep python-pynvim eslint_d
+	yay -S ccat ttf-meslo-nerd-font-powerlevel10k fpp-git urlview prettierd
+elif hash apt-get 2>/dev/null; then
+	sudo apt install -y thefuck exa fzf zsh tmux xclip ranger
 fi
 
 curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
