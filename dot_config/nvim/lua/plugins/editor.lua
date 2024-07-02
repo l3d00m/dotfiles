cc4cfbf1ccb6a3012465ffe30c5ffa8cf6de74d0
l3d00m <substantialimpulse@pm.me>
2024-07-02 17:36:38 +0200

    use persisted.nvim

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -2,4 +2,27 @@ return {
   {
     "chaoren/vim-wordmotion",
   },
+  { "folke/persistence.nvim", enabled = false },
+  -- { "nvimdev/dashboard-nvim", enabled = false },
+  {
+    "olimorris/persisted.nvim",
+    lazy = false, -- make sure the plugin is always loaded at startup
+    opts = {
+      use_git_branch = true,
+      default_branch = "master",
+      -- autoload = true,
+      autosave = true,
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>qs", function() require("persisted").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persisted").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persisted").stop() end, desc = "Don't Save Current Session" },
+      {
+        "<leader>fs",
+        "<cmd>Telescope persisted<cr>",
+        desc = "Find Session",
+      },
+    },
+  },
 }
