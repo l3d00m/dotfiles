40664e98474d0a4053b5dd2410dfccb9957c53fd
l3d00m <substantialimpulse@pm.me>
2024-08-21 11:45:44 +0200

    more plugin configs

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -5,12 +5,18 @@ return {
   { "folke/persistence.nvim", enabled = false },
   -- { "nvimdev/dashboard-nvim", enabled = false },
   {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+    },
+  },
+  {
     "olimorris/persisted.nvim",
     lazy = false, -- make sure the plugin is always loaded at startup
     opts = {
       use_git_branch = true,
       default_branch = "master",
-      -- autoload = true,
+      autoload = true,
       autosave = true,
     },
     -- stylua: ignore
@@ -41,8 +47,32 @@ return {
         pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
       },
       highlight = {
-        pattern = [[^[^a-zA-Z0-9\s]*<(KEYWORDS)\s]], -- match without the extra colon. You'll likely get false positives
+        pattern = [[(KEYWORDS)]],
       },
+    },
+  },
+  "akinsho/git-conflict.nvim",
+  lazy = false,
+  opts = {
+    default_mappings = {
+      ours = "<leader>gho",
+      theirs = "<leader>ght",
+      none = "<leader>gh0",
+      both = "<leader>ghb",
+      next = "]x",
+      prev = "[x",
+    },
+  },
+  keys = {
+    {
+      "<leader>gx",
+      "<cmd>GitConflictListQf<cr>",
+      desc = "List Conflicts",
+    },
+    {
+      "<leader>gr",
+      "<cmd>GitConflictRefresh<cr>",
+      desc = "Refresh Conflicts",
     },
   },
 }
