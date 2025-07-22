5a92e5d8b40e52f1982a6e82329a18b1035e42df
l3d00m <substantialimpulse@pm.me>
2025-07-22 12:03:29 +0200

    reduce install script

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
 
 if hash pacman 2>/dev/null; then
-  sudo pacman -S yay thefuck exa fzf zsh tmux xclip ranger neovim ripgrep python-pynvim eslint_d kitty
-  yay -S ccat ttf-meslo-nerd-font-powerlevel10k fpp-git urlview prettierd
-elif hash apt-get 2>/dev/null; then
-  sudo apt install -y thefuck exa fzf zsh tmux xclip ranger
+  sudo pacman -S yay thefuck exa fzf zsh tmux xclip ranger neovim ripgrep python-pynvim kitty
 fi
 
-curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+# curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
 # sudo chsh -s $(which zsh) $(whoami)
