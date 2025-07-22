533b40543e59daab5ec5607078bfaa10e3cbed3a
l3d00m <substantialimpulse@pm.me>
2025-07-22 12:03:29 +0200

    Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if hash pacman 2>/dev/null; then
-  sudo pacman -S yay thefuck exa fzf zsh tmux xclip ranger neovim ripgrep python-pynvim kitty
+  sudo pacman -S thefuck exa fzf zsh tmux xclip ranger neovim ripgrep python-pynvim kitty
 fi
 
 # curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
