4692fad41b9213e1f2434c932885ca86167f10ef
Thomas <substantialimpulse@pm.me>
2025-07-25 14:00:33 +0200

    autorandr improvements, install more packages

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if hash pacman 2>/dev/null; then
-    sudo pacman -S eza fzf zsh tmux xclip ranger neovim urlscan ripgrep python-pynvim kitty neomutt zoxide
+  sudo pacman -S eza fzf zsh tmux xclip ranger neovim urlscan ripgrep python-pynvim kitty neomutt zoxide nvm autorandr firefox polybar lazygit
 fi
 
 # curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
